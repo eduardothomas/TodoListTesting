70278d09d9c1fdddba32f7dc6962e54fe253ba45
Eduardo Thomas <eduardothomas64@gmail.com>
2024-03-01 20:07:15 -0500

    Initial Commit

---
+++ TodoListTesting/TodoListTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListTestingApp.swift
+//  TodoListTesting
+//
+//  Created by Eduardo Thomas on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
