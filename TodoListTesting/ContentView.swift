00819b575f59daaa07f7a6706683b2bff79cc127
Eduardo Thomas <eduardothomas64@gmail.com>
2024-03-08 11:03:08 -0500

    Update ContentView.swift

--- TodoListTesting/ContentView.swift
+++ TodoListTesting/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    //let textValue: String = "Hello Workshop"
+    @State var todos: [String] = [
+        "End world Hunger",
+        "Start strength training",
+        "Walk for 30 minutes",
+        "Solve the gravity equation",
+        "Read for 20 minutes",
+        "Go lift heavy items"
+    ]
+    
+    @State var showingAlert: Bool = false
+    @State var newItem = ""
+    
+    func addNewItem() {
+        print("Added item")
+        todos.append(newItem)
+        newItem = ""
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List($todos, id: \.self, editActions: .all) { $todoItem in
+                Text(todoItem)
+            }
+            .navigationTitle("To Do List")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                        showingAlert.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        }.alert("What do you need to do?", isPresented: $showingAlert) {
+            TextField("Add item here", text: $newItem)
+            Button {
+                addNewItem()
+            } label: {
+                Text("OK")
+            }
         }
-        .padding()
     }
 }
 
